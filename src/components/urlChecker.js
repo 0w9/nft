91ca34111a38b0d09c7f8319f42ba365e75b1da9
0w9 <79471472+0w9@users.noreply.github.com>
2022-03-14 12:16:18 +0100

    Reposit

--- src/components/urlChecker.js
+++ src/components/urlChecker.js
@@ -37,9 +37,10 @@ export default class urlChecker extends Component {
               <form>
                   <h1>Find similar nfts</h1>
                   <input type="text" placeholder="enter image url" id="url"></input>
-                  <input type="file" placeholder="open a file" id="file"></input>
                   <button type="button" onClick={checkUrl}>Check</button>
               </form>
+
+              <input type="file" placeholder="open a file" id="file"></input>
           </div>
       )
   }

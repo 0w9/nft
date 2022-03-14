cc65a6ddc84269226ae503094c1c3be2a4b8ab12
0w9 <79471472+0w9@users.noreply.github.com>
2022-03-14 11:48:24 +0100

    Removed unused imports.

--- src/components/urlChecker.js
+++ src/components/urlChecker.js
@@ -1,41 +1,45 @@
 import React, {
-    Component
+  Component
 } from 'react'
-import {useState} from 'react';
 
-import fetch from 'node-fetch';
 
 const axios = require('axios');
 
 export default class urlChecker extends Component {
-    render() {
-      
-        function checkUrl() {
-          const options = {
-            method: 'POST',
-            url: 'https://api.nftport.xyz/v0/recommendations/similar_nfts/urls',
-            headers: {'Content-Type': 'application/json', Authorization: ''},
-            data: {
-              url: 'https://supermariorun.com/assets/img/stage/obj_mode1.png',
-              page_number: 1,
-              page_size: 50
-            }
-          };
-          
-          axios.request(options).then(function (response) {
-            console.log(response.data);
-          }).catch(function (error) {
-            console.error(error);
-          });
-        }
+  render() {
 
-        return ( 
-            <div>
-                <form>
-                    <input type="text" placeholder="enter image url" id="url"></input>
-                    <button type="button" onClick={checkUrl}>Check</button>
-                </form>
-            </div>
-        )
-    }
+      function checkUrl() {
+        const options = {
+          method: 'POST',
+          url: 'https://api.nftport.xyz/v0/recommendations/similar_nfts/urls',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': '1970291d-6325-4d02-956e-bf97900bedd7'
+          },
+          data: {
+            url: document.getElementById("url").value,
+            page_number: 1,
+            page_size: 50
+          }
+        };
+
+        axios.request(options).then(function (response) {
+          response.data.nfts.forEach(x => {
+            console.log(x.metadata);
+          })
+        }).catch(function (error) {
+          console.error(error);
+        });
+      }
+
+      return (
+          <div>
+              <form>
+                  <h1>Find similar nfts</h1>
+                  <input type="text" placeholder="enter image url" id="url"></input>
+                  <button type="button" onClick={checkUrl}>Check</button>
+              </form>
+          </div>
+      )
+  }
 }
